4433399dae699f39d28b5751508c5cf93c03d5bd
CaptainGimmy <81749327+Captaingimmy@users.noreply.github.com>
2021-11-26 22:21:16 +0100

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+  int fibonacci[10], arr[10], k;
+  for(k=0;k<10;k++)
+  {
+      fibonacci[k] = 0;
+  }
+  fibonacci[0] = 1;
+  for(k=2;k<10;k++)
+  {
+      fibonacci[k] = fibonacci[k - 1] + fibonacci[k - 2];
+  }
+  for(k=1;k<10;k++)
+  {
+      cout <<  "\n" << fibonacci[k];
+  }
+}
